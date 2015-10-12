b9b0e9a07af704b7712181798856e51ff35adbe3
DemiPixel <luke5227@gmail.com>
2015-10-12 14:53:11 -0700

    Amazing changes. Beautiful. Added another joystick for mouse.

--- index.js
+++ index.js
@@ -15,12 +15,39 @@ http.listen(4000, function() {
 	console.log('Listening for socket.io on 4000');
 });
 
+require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+  console.log('addr: '+add);
+})
+
 var direction = {
 	left: false,
 	right: false,
 	forward: false,
 	back: false
 }
+
+var mouseSpeed = {
+	x: 0,
+	y: 0
+};
+var MOUSE_SPEED = 200;
+var relMousePos = {
+	x: 0,
+	y: 0
+}
+
+setInterval(function() {
+	if (!mouseSpeed.x && !mouseSpeed.y) return;
+	var pos = robot.getMousePos();
+	var newpos = {
+		x: pos.x + mouseSpeed.x/60*MOUSE_SPEED + relMousePos.x,
+		y: pos.y + mouseSpeed.y/60*MOUSE_SPEED + relMousePos.y
+	}
+	robot.moveMouse(Math.floor(newpos.x), Math.floor(newpos.y));
+
+	relMousePos.x = newpos.x - Math.floor(newpos.x);
+	relMousePos.y = newpos.y - Math.floor(newpos.y);
+}, 1/60);
 
 io.on('connection', function(socket) {
 	console.log('New connection');
@@ -63,6 +90,17 @@ io.on('connection', function(socket) {
 		//console.log(direction);
 	});
 
+	socket.on('movemouse', function(pos) {
+		console.log(pos);
+		if (pos.x > 1 || pos.x < -1 || pos.y > 1 || pos.y < -1) return;
+		if (Math.abs(pos.x) + Math.abs(pos.y) < 0.4) {
+			pos.x = 0;
+			pos.y = 0;
+		}
+		mouseSpeed.x = pos.x;
+		mouseSpeed.y = pos.y;
+	});
+
 	var buttons = {
 		jump: false,
 		crouch: false,
@@ -78,7 +116,7 @@ io.on('connection', function(socket) {
 	});
 
 	function buttonInteraction(button, press) {
-		console.log('button',button);
+		//console.log('button',button);
 		var found = false;
 		for (var b in buttons) {
 			if (b == button) {
@@ -113,4 +151,42 @@ app.get('/', function(req, res) {
 
 app.get('/qr', function(req, res) {
 	res.render('qr.ejs');
+});
+
+
+
+
+
+
+
+
+var ip = '';
+
+'use strict';
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      //console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      if (ifname == 'en0') {
+      	ip = iface.address;
+      	console.log(ip + ':3000');
+      }
+      //console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
 });
